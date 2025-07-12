be8c566732f609062036cde3595dc006bca56a69
abdulmajida <ah.abdulmajid123@gmail.com>
2025-07-12 01:10:50 -0800

    done styling for the boilerplate code using tailwind

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -1,7 +1,7 @@
 const CalendarPage =()=>{
     return(
-    <div>
-        <h1>Welcome to calendar page</h1>
+    <div className="min-h-screen flex items-center justify-center bg-green-300">
+        <h1 className="text-3xl font-bold">Welcome to calendar page</h1>
     </div>
     ); 
 };
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -16,17 +16,20 @@ const Login = () =>{
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h2>Clinic staff login</h2>
+        <div className="min-h-screen flex items-center justify-center bg-gray-900">
+            <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm space-y-4 ">
+                <h2 className="text=2xl font-bold text-center">Clinic staff login</h2>
 
-                 {error && <p>{error}</p>}
+                 {error && <p className="text-rose-500 text-center text-sm">{error}</p>}
 
-                 <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                 <input className="w-full px-4 py-2 rounded"
+                        type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
 
-                 <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                 <input className="w-full px-4 py-2 rounded"
+                        type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
 
-                 <button type="submit">login</button>
+                 <button className="w-full bg-pink-500 text-white py-2 rounded hover:bg-blue-300"
+                         type="submit">login</button>
             </form>
         </div>
     );
